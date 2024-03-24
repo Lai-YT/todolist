12080887e32237ca7ec4336b40cc6812f3283e38
Lai-YT <381xvmvbib@gmail.com>
2024-03-24 15:33:20 +0800

    Allow CORS for frontend connection

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-sql-driver/mysql v1.8.0
 	github.com/gorilla/mux v1.8.1
 	github.com/jinzhu/gorm v1.9.16
+	github.com/rs/cors v1.10.1
 	github.com/sirupsen/logrus v1.9.3
 )
 
--- todolist.go
+++ todolist.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/rs/cors"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -140,5 +141,11 @@ func main() {
 	router.HandleFunc("/todo", GetItems).Methods("GET")
 	router.HandleFunc("/todo/{id}", UpdateItem).Methods("POST")
 	router.HandleFunc("/todo/{id}", DeleteItem).Methods("DELETE")
-	http.ListenAndServe(":8000", router)
+
+	handler := cors.New(cors.Options{
+		// NOTE: "OPTIONS" is not included in comparison with the blog post since it's not necessary.
+		// See https://stackoverflow.com/questions/66926518/should-access-control-allow-methods-include-options.
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+	}).Handler(router)
+	http.ListenAndServe(":8000", handler)
 }
